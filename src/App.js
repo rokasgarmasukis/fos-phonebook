57630629e18830fd56464958232c0b823af97238
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-25 05:53:07 +0100

    Fetch initial users from db.json

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 
-import { useState } from "react";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      const data = response.data;
+      setPersons(data)
+    })
+  }, [])
 
   const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(search.toLowerCase())
   );
+
+
 
   return (
     <div>
